9f994a5d65e6127cdf01ef545d26cb23cee7a495
Ammar <raedamar52@gmail.com>
2025-05-12 21:23:25 +0300

    cabins page

--- src/features/cabins/EditCabin/EditCabin.tsx
+++ src/features/cabins/EditCabin/EditCabin.tsx
@@ -1,4 +1,4 @@
-import { editCabin, ImageFileType, CabinType } from "@/services";
+import { editCabin, ImageFileType, CabinType } from '@/services';
 import {
   Button,
   Dialog,
@@ -9,16 +9,13 @@ import {
   Stack,
   Textarea,
   FileUploadFileChangeDetails,
-} from "@chakra-ui/react";
-import { useFormik } from "formik";
-import { LuFileImage } from "react-icons/lu";
-import React, { useState } from "react";
-import InputField from "@/components/InputField/InputField";
-import { Tooltip } from "@/components/ui/tooltip";
-import {
-  cabinFormValidation,
-  formInitialValues,
-} from "../CreateCabin/CreateCabin";
+} from '@chakra-ui/react';
+import { useFormik } from 'formik';
+import { LuFileImage } from 'react-icons/lu';
+import React, { useState } from 'react';
+import InputField from '@/components/InputField/InputField';
+import { Tooltip } from '@/components/ui/tooltip';
+import { cabinFormValidation, formInitialValues } from '../CreateCabin/CreateCabin';
 
 const EditCabin = ({
   open,
@@ -42,17 +39,14 @@ const EditCabin = ({
 
   async function handleSubmit(values: CabinType) {
     try {
-      if (!formik.dirty || cabinValues === undefined || cabinId === undefined)
-        return;
+      if (!formik.dirty || cabinValues === undefined || cabinId === undefined) return;
       if (cabinValues.image !== values.image) {
         const file = values.image as ImageFileType;
         const bucketName = `object/cabin-images/${file.name}`;
         const filePath = `${import.meta.env.VITE_WILD_OASIS_STORAGE_URL}/${bucketName}`;
         const cabinData = { ...values, image: filePath };
-        console.log("photo", cabinData);
         await editCabin(cabinId, cabinData, bucketName, file);
       } else {
-        console.log("No photo");
         await editCabin(cabinId, values);
       }
       setOpen(false);
@@ -67,35 +61,29 @@ const EditCabin = ({
     if (!files?.acceptedFiles?.[0]) return;
 
     const file: File = files.acceptedFiles[0];
-    formik.setFieldValue("image", file);
+    formik.setFieldValue('image', file);
   };
 
   return (
     <Dialog.Root
-      placement="center"
-      size="xl"
+      placement='center'
+      size='xl'
       lazyMount
       open={open}
-      onOpenChange={(e) => setOpen(e.open)}
+      onOpenChange={e => setOpen(e.open)}
     >
       <Dialog.Backdrop />
       <Dialog.Positioner>
         <Dialog.Content>
           <Dialog.Header>
-            <Dialog.Title color="var(--color-grey-800)">
-              Edit cabin
-            </Dialog.Title>
+            <Dialog.Title color='var(--color-grey-800)'>Edit cabin</Dialog.Title>
           </Dialog.Header>
-          <Dialog.Body paddingTop="8">
+          <Dialog.Body paddingTop='8'>
             <form onSubmit={formik.handleSubmit}>
-              <Stack
-                gap="5"
-                css={{ "--field-label-width": "148px" }}
-                color="var(--color-grey-700)"
-              >
+              <Stack gap='5' css={{ '--field-label-width': '148px' }} color='var(--color-grey-700)'>
                 <InputField
-                  name="name"
-                  label="Cabin name"
+                  name='name'
+                  label='Cabin name'
                   value={formik.values.name}
                   errorMessage={formik.errors.name}
                   onChange={formik.handleChange}
@@ -105,9 +93,9 @@ const EditCabin = ({
                 <Separator />
 
                 <InputField
-                  type="number"
-                  name="maxCapacity"
-                  label="Maximum capacity"
+                  type='number'
+                  name='maxCapacity'
+                  label='Maximum capacity'
                   value={formik.values.maxCapacity}
                   errorMessage={formik.errors.maxCapacity}
                   onChange={formik.handleChange}
@@ -118,9 +106,9 @@ const EditCabin = ({
                 <Separator />
 
                 <InputField
-                  type="number"
-                  name="regularPrice"
-                  label="Regular price"
+                  type='number'
+                  name='regularPrice'
+                  label='Regular price'
                   value={formik.values.regularPrice}
                   errorMessage={formik.errors.regularPrice}
                   onChange={formik.handleChange}
@@ -131,9 +119,9 @@ const EditCabin = ({
                 <Separator />
 
                 <InputField
-                  type="number"
-                  name="discount"
-                  label="Discount"
+                  type='number'
+                  name='discount'
+                  label='Discount'
                   value={formik.values.discount}
                   errorMessage={formik.errors.discount}
                   onChange={formik.handleChange}
@@ -144,19 +132,19 @@ const EditCabin = ({
                 <Separator />
 
                 <Field.Root
-                  justifyContent="start"
-                  gap="6.2rem"
-                  orientation="horizontal"
+                  justifyContent='start'
+                  gap='6.2rem'
+                  orientation='horizontal'
                   invalid={!!formik.errors.description}
                   disabled={isLoading}
                 >
                   <Field.Label>Description</Field.Label>
                   <Textarea
-                    name="description"
-                    focusRingColor="var(--color-brand-600)"
-                    maxW="16.8rem"
-                    minH="fit-content"
-                    maxH="8lh"
+                    name='description'
+                    focusRingColor='var(--color-brand-600)'
+                    maxW='16.8rem'
+                    minH='fit-content'
+                    maxH='8lh'
                     onChange={formik.handleChange}
                     value={formik.values.description}
                   />
@@ -166,28 +154,24 @@ const EditCabin = ({
                 <Separator />
 
                 <Field.Root
-                  justifyContent="start"
-                  gap="6.2rem"
-                  orientation="horizontal"
-                  marginBottom="2.4rem"
+                  justifyContent='start'
+                  gap='6.2rem'
+                  orientation='horizontal'
+                  marginBottom='2.4rem'
                   disabled={isLoading}
                 >
                   <Field.Label>Cabin photo</Field.Label>
 
                   <FileUpload.Root
-                    accept="image/*"
-                    name="image"
-                    onFileChange={(files) => handleFileChange(files)}
-                    flexDirection="row"
-                    maxW="20rem"
+                    accept='image/*'
+                    name='image'
+                    onFileChange={files => handleFileChange(files)}
+                    flexDirection='row'
+                    maxW='20rem'
                   >
                     <FileUpload.HiddenInput />
                     <FileUpload.Trigger asChild>
-                      <Button
-                        variant="outline"
-                        size="md"
-                        color="var(--color-grey-700)"
-                      >
+                      <Button variant='outline' size='md' color='var(--color-grey-700)'>
                         <LuFileImage /> Upload Images
                       </Button>
                     </FileUpload.Trigger>
@@ -198,17 +182,17 @@ const EditCabin = ({
 
               <Dialog.Footer>
                 <Dialog.ActionTrigger asChild>
-                  <Button variant="outline">Cancel</Button>
+                  <Button variant='outline'>Cancel</Button>
                 </Dialog.ActionTrigger>
                 <Tooltip
-                  content="Change one field at least to edit the cabin"
+                  content='Change one field at least to edit the cabin'
                   disabled={isLoading || formik.dirty}
                 >
                   <Button
-                    type="submit"
-                    color="var(--color-grey-100)"
-                    bgColor="var(--color-brand-500)"
-                    _hover={{ bgColor: "var(--color-brand-700)" }}
+                    type='submit'
+                    color='var(--color-grey-100)'
+                    bgColor='var(--color-brand-500)'
+                    _hover={{ bgColor: 'var(--color-brand-700)' }}
                     disabled={isLoading || !formik.dirty}
                   >
                     Edit
