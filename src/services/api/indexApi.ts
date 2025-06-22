fc7f8cf5018b3e5998944cd077d54ee87bba61fd
Ammar <raedamar52@gmail.com>
2025-06-22 23:19:29 +0300

    fixed cabin image deleting functionality

--- src/services/api/indexApi.ts
+++ src/services/api/indexApi.ts
@@ -1,19 +1,16 @@
-import { Params } from "react-router";
-import { http } from "../HttpService";
-import { API_ENDPOINTS } from "@/utils/constants";
-import { toaster } from "@/components/ui/toaster";
-import axios from "axios";
+import { Params } from 'react-router';
+import { http } from '../HttpService';
+import { API_ENDPOINTS } from '@/utils/constants';
+import { toaster } from '@/components/ui/toaster';
+import axios from 'axios';
 
-export const getDataRange = async (
-  endpoint: "cabins" | "bookings",
-  customParam: Params | null,
-) => {
+export const getDataRange = async (endpoint: 'cabins' | 'bookings', customParam: Params | null) => {
   const countRes = await http.request<{ count: number }[]>(
-    "get",
+    'get',
     `${API_ENDPOINTS.base}/${endpoint}`,
     {
-      params: { select: "count", ...customParam },
-    },
+      params: { select: 'count', ...customParam },
+    }
   );
 
   return countRes.data[0].count;
@@ -22,21 +19,21 @@ export const getDataRange = async (
 export const postImage = async (bucketName: string, file: Blob) => {
   try {
     const formData = new FormData();
-    formData.append("file", file);
+    formData.append('file', file);
 
     const res = await http.request<{ Key: string }>(
-      "post",
-      `${API_ENDPOINTS.storage}/${bucketName.replaceAll(" ", "").toLowerCase()}`,
+      'post',
+      `${API_ENDPOINTS.storage}/${bucketName.replaceAll(' ', '').toLowerCase()}`,
       {
         data: formData,
-      },
+      }
     );
     return res.request.responseURL as string;
   } catch (err) {
     if (axios.isAxiosError(err)) {
       if (err) {
         toaster.error({
-          title: "Failed to upload image",
+          title: 'Failed to upload image',
           description: `${err.response?.data.message}`,
         });
       }
@@ -45,4 +42,22 @@ export const postImage = async (bucketName: string, file: Blob) => {
       Promise.reject(err);
     }
   }
+};
+
+export const deleteUniqueImage = async (imagePath: string, endpoint: string) => {
+  // To filter if any item in the table uses the same image
+  const res = await http.request<string[]>('get', endpoint, {
+    params: {
+      select: 'image',
+      image: `eq.${imagePath}`,
+    },
+  });
+  const rowsWithSameImg = res.data;
+  //  "rowsWithSameImg.length === 0" makes the delete request happen when no item in the table uses the imagePath
+  // That means you need to call the deleteUniqueImage function after the successful delete of the item in the server
+  if (rowsWithSameImg.length === 0) {
+    http.request<void>('delete', imagePath);
+  }
+
+  return rowsWithSameImg;
 };
